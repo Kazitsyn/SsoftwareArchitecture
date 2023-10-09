8533ede35e76af55d145cdab9885ff9493858daf
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-10-09 20:21:23 +0300

    HW12+

---
+++ HomeWork_11/src/test/java/ru/archetecture/hw11/serialization/MessageSerializerTest.java
@@ -0,0 +1,74 @@
+package ru.archetecture.hw11.serialization;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ru.archetecture.hw11.commands.base.CommandType;
+import ru.archetecture.hw11.message.LoginMessage;
+import ru.archetecture.hw11.message.Message;
+import ru.archetecture.hw11.message.SendMessage;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class MessageSerializerTest {
+
+    private final Map<CommandType, Message> messages = new HashMap<>();
+
+    @Before
+    public void setup() {
+        LoginMessage login = new LoginMessage();
+        login.setType(CommandType.USER_LOGIN);
+        login.setArgType(LoginMessage.ArgType.LOGIN);
+        login.setSender(123L);
+        login.setLogin("Jack");
+        login.setPass("qwerty");
+        messages.put(CommandType.USER_LOGIN, login);
+
+        SendMessage send = new SendMessage();
+        send.setType(CommandType.CHAT_SEND);
+        send.setSender(123L);
+        send.setChatId(1L);
+        send.setMessage("Hello world!");
+        messages.put(CommandType.CHAT_SEND, send);
+
+    }
+
+    @Test
+    public void encodeLogin() throws Exception {
+        Message origin = messages.get(CommandType.USER_LOGIN);
+
+        System.out.println(origin);
+
+        Protocol protocol = new SerializationProtocol();
+        byte[] data = protocol.encode(origin);
+        Message copy = protocol.decode(data);
+
+        System.out.println(copy);
+
+        assertTrue(!copy.equals(origin));
+    }
+
+
+    @Test
+    //@Ignore
+    public void encodeSend() throws Exception {
+        SendMessage origin = (SendMessage) messages.get(CommandType.CHAT_SEND);
+
+        System.out.println(origin);
+
+        Protocol protocol = new SerializationProtocol();
+        byte[] data = protocol.encode(origin);
+        SendMessage copy = (SendMessage) protocol.decode(data);
+
+        System.out.println(copy);
+
+        assertTrue(!copy.equals(origin));
+    }
+}
